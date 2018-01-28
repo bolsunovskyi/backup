b83ae6a6782d3aaa6555a9c8030b2f3137824dbe
Michael B <dev@mihael.me>
2018-01-28 20:15:46 +0200

    initial commit

---
+++ src/com/backup/App.java
@@ -0,0 +1,51 @@
+package com.backup;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class App {
+    private JPanel main_panel;
+    private JList <String>folder_list;
+    private JButton addFolder;
+    private JPanel bar_panel;
+    private JLabel bar_label;
+    private JTextArea log;
+    private DefaultListModel<String> listModel;
+
+    private App() {
+        listModel = new DefaultListModel<>();
+        folder_list.setModel(listModel);
+
+        addFolder.addActionListener((ActionEvent e) -> {
+            JFileChooser fc = new JFileChooser();
+            fc.setCurrentDirectory(new java.io.File("."));
+            fc.setDialogTitle("Choose folder to backup");
+            fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            fc.setAcceptAllFileFilterUsed(false);
+            if (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                String path = fc.getSelectedFile().getAbsolutePath();
+
+                listModel.addElement(path);
+                bar_label.setText(path + " scanning...");
+                Scanner s = new Scanner(path, this);
+                new Thread(s).start();
+            }
+        });
+    }
+
+    public void scanDone(String folder) {
+        bar_label.setText(folder + " done.");
+    }
+
+    public void scanFile(String file) {
+        log.append(file + "\r\n");
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().main_panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+}
---
+++ src/com/backup/Scanner.java
@@ -0,0 +1,47 @@
+package com.backup;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scanner implements Runnable {
+    private String folder;
+    private List<String> files;
+    private App app;
+    
+    Scanner(String folder, App app) {
+        this.files = new ArrayList<>();
+        this.folder = folder;
+        this.app = app;
+    }
+
+    public List<String> getFiles() {
+        return this.files;
+    }
+
+    public void run() {
+        File startFolder = new File(folder);
+        scanFolder(startFolder);
+        app.scanDone(startFolder.getAbsolutePath());
+    }
+    
+    private void scanFolder(File folder) {
+        if (!folder.isDirectory()) {
+            return;
+        }
+
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File f : files) {
+                if (f.isFile()) {
+                    String path = f.getAbsolutePath();
+                    //this.files.add(path);
+                    System.out.println(path);
+                    this.app.scanFile(path);
+                } else if(f.isDirectory()) {
+                    scanFolder(f);
+                }
+            }
+        }
+    }
+}
